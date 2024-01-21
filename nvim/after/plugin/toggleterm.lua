f5063be04385f2fde085905cd217e13bc0ac1092
vangxitrum <tuantq666@gmail.com>
2024-01-21 08:36:36 -0800

    initial commit

---
+++ nvim/after/plugin/toggleterm.lua
@@ -0,0 +1,26 @@
+local status_ok, toggleterm = pcall(require, "toggleterm")
+if not status_ok then
+    return
+end
+
+toggleterm.setup({
+    size = 20,
+    open_mapping = [[<c-\>]],
+    hide_numbers = true,
+    shade_filetypes = {},
+    shade_terminals = true,
+    start_in_insert = true,
+    insert_mappings = true,
+    persist_size = true,
+    direction = "horizontal",
+    close_on_exit = true,
+    shell = vim.o.shell,
+    float_opts = {
+        border = "curved",
+        winblend = 0,
+        highlights = {
+            border = "Normal",
+            background = "Normal",
+        }
+    },
+})
